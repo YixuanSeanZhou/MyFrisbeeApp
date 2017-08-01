7e924181e65c399aad77ad520a5fbd4d72913839
GaryWang2333 <wpp2333@gmail.com>
2017-08-01 16:23:04 +0800

    finally merged

---
+++ MyFrisbeeApp/MyFrisbeeApp/Controller/CreateAGame.swift
@@ -0,0 +1,38 @@
+//
+//  creayeAGameViewController.swift
+//  Frisbee Community
+//
+//  Created by gaoqingli on 2017/8/1.
+//  Copyright © 2017年 Makeschool. All rights reserved.
+//
+
+import UIKit
+
+class CreateAGameViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    @IBOutlet weak var descriptionTextView: UITextView!
+    @IBOutlet weak var descriptionImageView: UIImageView!
+    @IBOutlet weak var titleTextView: UITextView!
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+}
